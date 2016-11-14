c3b4cd17e6f1dc933c0a27aca73d093d834c6ccc
Avinash <avinash.jairam@gmail.com>
2016-11-13 22:00:29 -0500

    Working on file input. Currently don't know where to catch exception

--- CsvFileJoiner.java
+++ CsvFileJoiner.java
@@ -1,16 +1,63 @@
 import java.io.*;
+import java.util.*;
 
 public class  CsvFileJoiner{
 	
+	private File file1;
+	private File file2;
+	private File file3;
+	private File file4;
+	private File file5;
+	private File file6;
+	
+	private Scanner input1;
+	private Scanner input2;
+	private Scanner input3;
+	private Scanner input4;
+	private Scanner input5;
+	private Scanner input6;
 	
 	public CsvFileJoiner(){
-
+		initializeInputObjects();
 	}
 
-		
+	public void initializeInputObjects() throws FileNotFoundException{
+
+		try{
+			file1 = new File("dividends.head");
+			file2 = new File("splits.head");
+			file3 = new File("cts.head");
+			file4 = new File("dividends.dump.csv");
+			file5 = new File("splits.dump.csv");
+			file6 = new File("cts.dump.csv");
+			
+			input1 = new Scanner(file1);
+			input2 = new Scanner(file2);
+			input3 = new Scanner(file3);
+			input4 = new Scanner(file4);
+			input5 = new Scanner(file5);
+			input6 = new Scanner(file6);
+		}
+		catch (FileNotFoundException ex){
+			System.out.println("Unable to read file");
+
+		}
+
+
+
+
+	}		
 
 	
 	public static void main(String [] args){
+		CsvFileJoiner obj = new CsvFileJoiner();
+		obj.selectJoinType(args);
+		obj.readFile();
+
+
+	}
+
+	public void selectJoinType(String [] args){
 		if("merge_join".equals(args[0])){
 			System.out.println("merge join");
 		}
@@ -23,14 +70,13 @@ public class  CsvFileJoiner{
 		else{
 			System.out.println("Invalid Input! Please enter the correct join, for example, \'merge_join\'");
 		}
-		
-
-
 	}
 
-
-
-
+	public  void readFile(){
+		while(input1.hasNext()){
+			System.out.println(input1.next());
+		}
+	}
 
 
 }
