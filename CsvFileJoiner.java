7cdcb6e910ec1bcb1bb839a8175042f6dec8fb94
Avinash <avinash.jairam@gmail.com>
2016-11-23 00:18:25 -0500

    Project specs changed. Only have to join 2 csv files. Created 3 simple test files - input1.csv, input2.csv, input3.csv

--- CsvFileJoiner.java
+++ CsvFileJoiner.java
@@ -9,60 +9,49 @@ public class  CsvFileJoiner{
 	
 	private File file1;
 	private File file2;
-	private File file3;
-	private File file4;
-	private File file5;
-	private File file6;
 	
-	private Scanner input1;
-	private Scanner input2;
-	private Scanner input3;
-	private Scanner input4;
-	private Scanner input5;
-	private Scanner input6;
+	private Scanner stdin1;
+	private Scanner stdin2;
 	
-	public CsvFileJoiner(){
-		initializeInputObjects();
+	public CsvFileJoiner(String input1, String input2){
+		initializeInputObjects(input1, input2);
 	}
 
-	public void initializeInputObjects(){
+	public void initializeInputObjects(String input1, String input2){
 
 		try{
-			file1 = new File("dividends.head");
-			file2 = new File("splits.head");
-			file3 = new File("cts.head");
-			file4 = new File("dividends.dump.csv");
-			file5 = new File("splits.dump.csv");
-			file6 = new File("cts.dump.csv");
+			file1 = new File(input1);
+			file2 = new File(input2);
 			
-			input1 = new Scanner(file1);
-			input2 = new Scanner(file2);
-			input3 = new Scanner(file3);
-			input4 = new Scanner(file4);
-			input5 = new Scanner(file5);
-			input6 = new Scanner(file6);
+			stdin1 = new Scanner(file1);
+			stdin2 = new Scanner(file2);
+	
+
+		//	System.out.println(stdin2.next());	
 		}
 		catch (FileNotFoundException ex){
 			System.out.println("Unable to read file");
-
 		}
-
-
-
-
 	}		
 
 	
 	public static void main(String [] args){
-		CsvFileJoiner obj = new CsvFileJoiner();
-		obj.selectJoinType(args);
-		obj.readFile();
-
+		try {
+			CsvFileJoiner obj = new CsvFileJoiner(args[1],args[2]);
+			obj.selectJoinType(args[0]);
+		
+			//System.out.println(obj.getKey("cow,horse"));
+			//System.out.println(args[0] + " " + args[1] + " " + args[2]);
+			//obj.readFile();
+		}
+		catch (Exception ex){
+			System.out.println(ex.getMessage());
+		}
 	
 		Connection c = null;
 		Statement stmt = null;
 
-	
+		/*	
 		try{
 			Class.forName("org.postgresql.Driver");
 			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/avi", "avi", "avi");
@@ -87,19 +76,20 @@ public class  CsvFileJoiner{
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 			System.exit(0);
 		}
-		System.out.println("Opened database successfully");	
+		System.out.println("Opened database successfully");	*/
 
 
 	}
 
-	public void selectJoinType(String [] args){
-		if("merge_join".equals(args[0])){
+	public void selectJoinType(String  type){
+		if("merge_join".equals(type)){
 			System.out.println("merge join");
 		}
-		else if ("inner_loop_join".equals(args[0])){
-			System.out.println("inner loop join");
+		else if ("inner_loop_join".equals(type)){
+			innerLoopJoin();
+			System.out.println(type);
 		}
-		else if("hash_join".equals(args[0])){
+		else if("hash_join".equals(type)){
 			System.out.println("hash join");
 		}
 		else{
@@ -108,27 +98,56 @@ public class  CsvFileJoiner{
 	}
 
 	public  void readFile(){
-		/*while(input1.hasNext()){
-			System.out.println(input1.next());
+		while(stdin1.hasNext()){
+			
 		}
 
-		while(input2.hasNext()){
-			System.out.println(input2.next());
+		while(stdin2.hasNext()){
+			System.out.println(stdin2.next());
 		}
 
-		while(input3.hasNext()){
-			System.out.println(input3.next());
-		}
-
-		while(input2.hasNext()){
-			System.out.println(input2.next());
-		}*/
+		/*
 
 		int count = 0;
 
 		while(count<10){
 			System.out.println(input6.next());
 			count++;
-		}
+		} */
 	}
+
+	public String getKey(String value){
+		return value.substring(0,value.indexOf(","));
+	}
+
+	public String getMatchingRecord(String value){
+		return value.substring(value.indexOf(",") + 1 );
+	}
+
+
+
+	public void innerLoopJoin(){
+		while(this.stdin1.hasNext()){
+			while(this.stdin2.hasNext()){
+				if( this.getKey(stdin1.next()).equals( this.getKey(stdin2.next())) ){
+				//	System.out.println("ehy");
+				//	System.out.println(this.getKey(stdin1.next()) + " " + this.getKey(stdin2.next()) );
+				//	this.getKey(stdin1.next()).equals( this.getKey(stdin2.next())
+				//System.out.println(this.getKey(stdin1.next()) + " " + this.getMatchingRecord(stdin1.next()) + " " + this.getMatchingRecord(stdin2.next()) );
+				System.out.println(this.stdin2.next());	
+			}
+			//	System.out.println(this.stdin1.next() + " " + this.stdin2.next());
+			//	System.out.println(this.getKey(stdin1.next()) + " " + this.getKey(stdin2.next()));
+			}
+		}
+		
+	}	
+		
+
+
+
+
+
 }
+
+
